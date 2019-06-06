bb6993094bbd55bf02c420ec9b60d05917daedf8
geweuwa <51253825+geweuwa@users.noreply.github.com>
2019-06-06 16:18:56 -0500

    Add files via upload

--- rps.py
+++ rps.py
@@ -1,5 +1,21 @@
 from turn import *
 
-user_turn.rps_check()
+user_input = user_turn.rps_check()
 
-print(history)
+#first round only, random choice
+
+t1 = random.randint(1,2)
+if t1 == 1:
+    bot_choice = "s"
+elif t1 == 2:
+    bot_choice = "r"
+
+def win_cond():
+        if user_input == "r" and bot_choice == "s":
+            print("You win!")
+        elif user_input == "r" and bot_choice == "r":
+            print("Tie.")
+        elif user_input == "r" and bot_choice == "p":
+            print("You lose.")
+
+win_cond()
